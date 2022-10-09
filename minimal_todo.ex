b7d427febad7626da0b0612593f208596caa60d8
Rafael Veiga <dsg.rafael@gmail.com>
2022-10-08 23:04:30 -0300

    add delete todo, get command to minimal todo

--- minimal_todo.ex
+++ minimal_todo.ex
@@ -11,7 +11,34 @@ defmodule MinimalTodo do
     # read todos, add todos, delete todos, load file, save file
   end
 
-  def get_command() do
+  def get_command(data) do
+    command =
+      IO.gets(
+        "What is your command? (R)ead todos, (A)dd a todo, (D)elete a todo, (L)oad a .csv, (S)ave a .csv\n"
+      )
+      |> String.trim()
+      |> String.downcase()
+
+    case command do
+      "r" -> show_todos(data)
+      "d" -> delete_todo(data)
+      "q" -> "Quit"
+      _ -> get_command(data)
+    end
+  end
+
+  def delete_todo(data) do
+    todo_do_delete = IO.gets("Which todo would you like to delete? \n") |> String.trim()
+
+    if Map.has_key?(data, todo_do_delete) do
+      new_data = Map.drop(data, [todo_do_delete])
+      IO.puts(~s("Todo "#{todo_do_delete}" has been deleted."))
+      get_command(new_data)
+    else
+      IO.puts("Todo not found\n")
+      show_todos(data, false)
+      delete_todo(data)
+    end
   end
 
   def read(filename) do
@@ -45,5 +72,9 @@ defmodule MinimalTodo do
     IO.puts("You have the following todos: \n")
     Enum.each(items, fn item -> IO.puts(item) end)
     IO.puts("\n")
+
+    if next? do
+      get_command(data)
+    end
   end
 end
